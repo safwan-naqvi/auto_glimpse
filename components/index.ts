269bc3d177f088148a309510e14b7886db5e2b4f
Syed Safwan Abbas <safwanabbas2020@gmail.com>
2023-11-07 01:49:42 +0500

    AutoGlimps Project Basic Architecture

---
+++ components/index.ts
@@ -0,0 +1,5 @@
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import CustomButton from "./CustomButton";
+export { Hero, Navbar, Footer, CustomButton };
